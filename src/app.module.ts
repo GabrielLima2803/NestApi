cf85cf214af2dbe1785ccc91909134f67a6ea413
GabrielLima2803 <gabriellima2803@gmail.com>
2024-04-27 22:13:16 -0300

    feat: Adição a CategoriaModule em AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/user.module';
+import { CategoriaModule } from './categorias/categoria.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, CategoriaModule],
   controllers: [AppController],
   providers: [AppService],
 })
