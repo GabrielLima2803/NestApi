5f7fce7ec9832f4ca2d97194b8ca0de1c377d7e4
GabrielLima2803 <gabriellima2803@gmail.com>
2024-05-02 11:59:30 -0300

    feat: Criação dos endpoits POST, PUT e DELETE

--- src/posts/post.controller.ts
+++ src/posts/post.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { PostService } from "./post.service";
+import { CreatePost } from "./dtos/create-post.dto";
+import { UpdatePost } from "./dtos/update-post.dto";
 
 @Controller("post")
 export class PostController {
@@ -7,5 +9,20 @@ export class PostController {
     @Get()
     async getAllPosts() {
         return await this.postService.findAllPosts();
+    }
+
+    @Post()
+    async createPost(@Body() createPost:CreatePost){
+        return await this.postService.createPost(createPost)
+    }
+
+    @Put(':id')
+    async putPost(@Param('id', ParseIntPipe) id:number, @Body() updatePost:UpdatePost){
+        return await this.postService.updatePost(id, updatePost)
+    }
+
+    @Delete(':id')
+    async deletePost(@Param('id', ParseIntPipe) id:number) {
+        return await this.postService.deletePost(id)
     }
 }
--- src/posts/post.service.ts
+++ src/posts/post.service.ts
@@ -1,11 +1,92 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { CreatePost } from "./dtos/create-post.dto";
+import { UpdatePost } from "./dtos/update-post.dto";
 
 @Injectable()
 export class PostService {
     constructor(private prisma: PrismaService) {}
     async findAllPosts() {
-        const posts = await this.prisma.post.findMany()
-        return posts
+        try {
+            const posts = await this.prisma.post.findMany({
+                include: {
+                    categoria: true
+                }
+            });
+            return posts;
+        } catch (error) {
+            console.error("Erro ao buscar os posts:", error);
+            throw error;
+        }
+    }
+    async createPost(post: CreatePost) {
+        try {
+            const findUniquePost = await this.prisma.post.findFirst({
+                where: {
+                    titulo: post.titulo
+                }
+            });
+            
+            if (findUniquePost) {
+                throw new Error("Post já existe");
+            }
+            
+            const createdPost = await this.prisma.post.create({
+                data: {
+                    titulo: post.titulo,
+                    publicado: post.publicado,
+                    categoria: {
+                        connect: { id: post.categoria.id } 
+                    }
+                }
+            });
+            
+            return createdPost;
+        } catch (error) {
+            console.error("Erro ao criar o post:", error);
+            throw error;
+        }
+    }
+
+    async updatePost(id:number, updatePost:UpdatePost) {
+
+        const findPost = await this.prisma.post.findUnique({
+            where: {
+                id: id
+            }
+        })
+        if (!findPost) {
+            throw new Error(`Post com ID ${id} não encontrada.`);
+        }
+        const updateDataPost = await this.prisma.post.update({
+            where: {
+                id: id
+            },
+            data: {
+                titulo: updatePost.titulo,
+                publicado: updatePost.publicado,
+                categoria: updatePost.categoria ? { connect: { id: updatePost.categoria.id } } : undefined
+
+            }
+        })
+        return updateDataPost;
+    }
+
+    async deletePost(id:number){
+        const findUniquePost = await this.prisma.post.findUnique({
+            where: {
+                id: id
+            }
+        })
+        if (!findUniquePost) {
+            throw new Error(`Post com ID ${id} não encontrada.`);
+        }
+
+        const deletePost = await this.prisma.post.delete({
+            where: {
+                id: id
+            }
+        })
+        return deletePost
     }
 }
